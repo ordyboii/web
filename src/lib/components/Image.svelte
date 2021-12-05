3cbe1ee4d30700a86e98f16dd81ff29b2d8afa36
Jake <jake.ord345@gmail.com>
2021-12-05 20:34:48 +0000

    changes

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,15 +1,16 @@
 <script>
   export let source;
+  const { fields } = source;
 </script>
 
 <picture>
-  <source srcset={`${source.url}?fm=avif&q=50`} type="image/avif" />
-  <source srcset={`${source.url}?fm=webp&q=50`} type="image/webp" />
+  <source srcset={`${source.url || fields?.file?.url}?fm=avif&q=50`} type="image/avif" />
+  <source srcset={`${source.url || fields?.file?.url}?fm=webp&q=50`} type="image/webp" />
   <img
-    src={`${source.url}?q=50`}
-    alt={source?.title || source?.description}
-    width={source?.width}
-    height={source?.height}
+    src={`${source.url || fields?.file?.url}?q=50`}
+    alt={source.alt || fields?.title}
+    width={fields?.file?.details.width}
+    height={fields?.file?.details.height}
     loading="lazy"
     decoding="async"
   />
