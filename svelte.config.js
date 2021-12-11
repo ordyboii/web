9b38676bb0eab57ee9db6ac290f08c5133c08e27
Jake <jake.ord345@gmail.com>
2021-12-11 15:28:03 +0000

    change

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import Static from '@sveltejs/adapter-static'
+import Static from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: Static(),
+    adapter: Static({
+      assets: 'build',
+      pages: 'build',
+      fallback: null
+    }),
     target: '#svelte'
   }
-}
+};
 
-export default config
+export default config;
