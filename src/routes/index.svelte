591b02aa2cdb655ab5a364533b5e65666bb17abb
Jake <jake.ord345@gmail.com>
2022-04-10 19:13:28 +0100

    hello

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,22 @@
   export let projects: Project[];
 </script>
 
+<svelte:head>
+  <script
+    src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+  <script>
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  </script>
+</svelte:head>
+
 <Hero />
 <Bio />
 <Featured
