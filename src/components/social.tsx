6f4525a458901ca3ce7b68b94dbe022966a83164
Jake <jake.ord345@gmail.com>
2022-06-05 14:13:36 +0100

    fixed responsive styling, added instagram

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -1,13 +1,28 @@
 import { Fragment } from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Social() {
   return (
     <Fragment>
-      <a href='/awd' className='hover:opacity-60 focus:opacity-60'>
+      <a
+        href='https://www.linkedin.com/in/jake-o-853626196'
+        className='hover:opacity-60 focus:opacity-60'
+        aria-label='Link to my LinkedIn page'
+      >
         <FaLinkedin size={24} />
       </a>
-      <a href='/awd' className='hover:opacity-60 focus:opacity-60'>
+      <a
+        href='https://www.instagram.com/jakeorddesigns'
+        className='hover:opacity-60 focus:opacity-60'
+        aria-label='Link to my Instagram page'
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a
+        href='https://github.com/jorddy'
+        className='hover:opacity-60 focus:opacity-60'
+        aria-label='Link to my Github page'
+      >
         <FaGithub size={24} />
       </a>
     </Fragment>
