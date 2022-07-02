db23db71a60804377ea5a02df3fc443ebd581157
Jake <jake.ord345@gmail.com>
2022-07-02 12:30:52 +0100

    upgrade to next 12

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  experimental: {
+    newNextLinkBehavior: true,
+    images: {
+      allowFutureImage: true
+    }
+  }
+};
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/future/image";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 export default function About() {
@@ -7,14 +7,11 @@ export default function About() {
       <div className='space-y-6'>
         <h2 className='text-2xl font-bold'>A little about me</h2>
 
-        <div className='border-4 border-gray-900 relative w-3/4 h-64 sm:w-1/4'>
-          <Image
-            src='/images/me2.jpg'
-            alt='Jake Ord standing on some stairs in Edinburgh'
-            layout='fill'
-            objectFit='cover'
-          />
-        </div>
+        <Image
+          className='border-4 border-gray-900 object-cover h-64'
+          src='/images/me2.jpg'
+          alt='Jake Ord standing on some stairs in Edinburgh'
+        />
 
         <p className='font-semibold'>
           初めましてジェイクです。 (I&apos;m Jake, nice to meet you)
@@ -43,7 +40,7 @@ export default function About() {
 
         <p>
           But who works all the time, right? In my downtime I&apos;m either
-          binging Netflix, collecting figures, trying to keep my Duolingo streak
+          binging Netflix, collecting figures, trying to beat my Duolingo streak
           or cooking some crazy new dish I thought of.
         </p>
 
