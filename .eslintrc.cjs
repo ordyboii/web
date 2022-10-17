ac4a19027a8c3a14e8173a28342eeadac90afa5e
Jake <jake.ord345@gmail.com>
2022-10-17 19:15:02 +0100

    setup eslint prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["plugin:astro/recommended", "plugin:tailwindcss/recommended"],
+  plugins: ["tailwindcss"],
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"]
+      }
+    }
+  ]
+};
