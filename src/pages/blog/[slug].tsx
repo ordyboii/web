63ed0818a21130c493c08d2fd64a6d2a78a55019
Jake <jake.ord345@gmail.com>
2022-10-21 00:36:46 +0100

    fixed build error

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,31 +1,3 @@
-import SEO from "components/seo";
-import { HeadingOne } from "components/typography";
-import type { InferGetStaticPropsType } from "next";
-import { useRef } from "react";
-import { useAnnotation } from "utils/annotation";
-import { getContent, type Post } from "utils/markdown";
-
-export const getStaticProps = () => {
-  const posts = getContent<Post["data"]>("posts");
-
-  return {
-    props: { posts }
-  };
-};
-
-type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>;
-
-export default function BlogPage({ posts }: BlogPageProps) {
-  const headingRef = useRef<HTMLHeadingElement>(null);
-  useAnnotation(headingRef, "box");
-
-  return (
-    <>
-      <SEO title='Jake Ord - Blog' />
-      <section className='grid gap-6 py-16'>
-        <HeadingOne ref={headingRef}>Blog</HeadingOne>
-        {/* <BlogsGrid Blogs={Blogs} /> */}
-      </section>
-    </>
-  );
+export default function Post() {
+  return <h1>Hello</h1>;
 }
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,31 @@
-export default function BlogPage() {
-  return <h1>Hello</h1>;
+import SEO from "components/seo";
+import { HeadingOne } from "components/typography";
+import type { InferGetStaticPropsType } from "next";
+import { useRef } from "react";
+import { useAnnotation } from "utils/annotation";
+import { getContent, type Post } from "utils/markdown";
+
+export const getStaticProps = () => {
+  const posts = getContent<Post["data"]>("posts");
+
+  return {
+    props: { posts }
+  };
+};
+
+type BlogPageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function BlogPage({ posts }: BlogPageProps) {
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  useAnnotation(headingRef, "box");
+
+  return (
+    <>
+      <SEO title='Jake Ord - Blog' />
+      <section className='grid gap-6 py-16'>
+        <HeadingOne ref={headingRef}>Blog</HeadingOne>
+        {/* <BlogsGrid Blogs={Blogs} /> */}
+      </section>
+    </>
+  );
 }
