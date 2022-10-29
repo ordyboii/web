53ad2f65e7c3d845a67e24b1513b3e249cc4cd72
Jake <jake.ord345@gmail.com>
2022-10-29 13:17:11 +0100

    removed nextjs project

--- next-js/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @param {import('next').NextConfig} config */
-const defineConfig = config => config;
-
-export default defineConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: { appDir: true }
-});
