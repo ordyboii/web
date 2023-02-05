1d0d1d7a63196a74b6e786cddd42d85db546a5ac
Jake <jake.ord345@gmail.com>
2023-02-05 20:58:58 +0000

    better styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({ mode: "standalone" })
+  adapter: node({ mode: "standalone" }),
+  server: { host: "0.0.0.0" }
 });
