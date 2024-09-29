9725beb8b56388503d1fae3cf574c8fa2983fa6b
ordyboii <68291857+ordyboii@users.noreply.github.com>
2024-09-29 16:35:52 +0100

    removal of js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["nuxt-svgo"],
   routeRules: {
-    "/": { prerender: true },
+    "/": {
+      prerender: true,
+      experimentalNoScripts: true,
+    },
   },
 });
